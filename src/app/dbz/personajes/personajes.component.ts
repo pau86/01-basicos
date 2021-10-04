6c1c22f9c593ec2e36b959493819dc1797a49372
Paulina Pérez <paulinabp8601@gmail.com>
2021-10-04 12:25:58 +0200

    Until video 62 - Create Personajes component

---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html'
+})
+export class PersonajesComponent {
+
+  @Input() personajes: Personaje[] = []; // Los personajes vienen del componente padre. @Input se usa para pasar información de un componente padre a un componente hijo.
+
+  // @Input("data") personajes: any[] = []; // Si queremos cambiarle el nombre de personajes a data lo podemos hacer de esta manera, pero en este caso el html también tendrá que llamar a data y no a personajes
+
+}
