0b24c7903b44df0d23f503b2cb8be07e47d5be79
Paulina Pérez <paulinabp8601@gmail.com>
2021-10-06 13:41:19 +0200

    Limpieza previa

--- src/app/dbz/personajes/personajes.component.ts
+++ src/app/dbz/personajes/personajes.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-// import { Personaje } from '../interfaces/dbz.interface';
+import { Component } from '@angular/core';
+
 import { DbzService } from '../services/dbz.service';
 
 @Component({
@@ -7,10 +7,6 @@ import { DbzService } from '../services/dbz.service';
   templateUrl: './personajes.component.html'
 })
 export class PersonajesComponent {
-
-  // @Input() personajes: Personaje[] = []; // Los personajes vienen del componente padre. @Input se usa para pasar información de un componente padre a un componente hijo.
-
-  // @Input("data") personajes: any[] = []; // Si queremos cambiarle el nombre de personajes a data lo podemos hacer de esta manera, pero en este caso el html también tendrá que llamar a data y no a personajes
 
   get personajes() {
     return this.dbzService.personajes;
