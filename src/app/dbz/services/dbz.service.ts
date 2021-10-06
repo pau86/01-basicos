5ff098362e07fd0c7d0a5a7ddf01d6e160d9eafc
Paulina Pérez <paulinabp8601@gmail.com>
2021-10-06 13:40:25 +0200

    Video 67 - Métodos en el servicio | Agregar nuevo personaje

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -27,8 +27,10 @@ export class DbzService {
         return [...this._personajes];
     }
 
-    constructor() {
-        console.log("Servicio inicializado");
+    constructor() {}
+
+    agregarPersonaje( personaje: Personaje ) {
+        this._personajes.push( personaje );
     }
 
 }
